47ca2783828701a4037148498b78a1b7e2b43920
danieldolz <danieldolz@iesebre.com>
2016-10-27 21:55:35 +0200

    layouts2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,34 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Adminlte Tinkering</title>
 
-    <link rel="stylesheet" href="css/all.css">
-</head>
+@section('htmlheader')
+    @include('partials.htmlheader')
+@show
 
-<body>
-@yield('content')
+<body class="hold-transition skin-blue sidebar-mini">
+
+<div class="wrapper" id="app">
+    @include('partials.mainheader')
+
+    @include('partials.sidebar')
+
+    <div class="content-wrapper">
+        @include('partials.contentheader')
+        <section class="content">
+            @yield('content')
+        </section>
+    </div>
+
+    @include('partials.controlsidebar')
+
+    @include('partials.footer')
+</div>
+
+
+@section('scripts')
+    @include('partials.scripts')
+@show
+
 </body>
-
 
 </html>
