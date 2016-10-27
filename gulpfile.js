298c7e22f238772dc408bd523a46940166c8e9b9
danieldolz <danieldolz@iesebre.com>
2016-10-28 00:06:36 +0200

    css i img arreglats

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,7 @@ require('laravel-elixir-vue-2');
 elixir(mix => {
     mix.sass('app.scss')
         .less('app-less.less')
-        .less('./node_modules/admin-lte/build/less/AdminLTE.less',
-            './public/css/admin-lte.css')
+        .less('./node_modules/admin-lte/build/less/AdminLTE.less','./public/css/adminlte-less.css')
         .styles([
             './public/css/app.css',
             './public/css/app-less.css',
@@ -29,26 +28,4 @@ elixir(mix => {
         .copy('node_modules/admin-lte/bootstrap/fonts/*.*','public/fonts/bootstrap')
         .copy('node_modules/admin-lte/dist/img','public/img')
         .webpack('app.js');
-
-// .less('./node_modules/admin-lte/build/less/AdminLTE.less', './public/css/adminlte-less.css')
-//     .less('adminlte-app-less.less')
-//     .less('./node_modules/toastr/toastr.less')
-//     .styles([
-//         './public/css/app.css',
-//         './node_modules/admin-lte/dist/css/skins/_all-skins.css',
-//         './public/css/adminlte-less.css',
-//         './public/css/adminlte-app.css',
-//         './node_modules/icheck/skins/square/blue.css',
-//         './public/css/toastr.css',
-//     ])
-//     .copy('node_modules/font-awesome/fonts/*.*','public/fonts/')
-//     .copy('node_modules/ionicons/dist/fonts/*.*','public/fonts/')
-//     .copy('node_modules/admin-lte/bootstrap/fonts/*.*','public/fonts/bootstrap')
-//     .copy('node_modules/admin-lte/dist/css/skins/*.*','public/css/skins')
-//     .copy('node_modules/admin-lte/dist/img','public/img')
-//     .copy('node_modules/admin-lte/plugins','public/plugins')
-//     .copy('node_modules/icheck/skins/square/blue.png','public/css')
-//     .webpack('app.js');
-
-
 });
